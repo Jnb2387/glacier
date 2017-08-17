4ac15a38fb77a1ae7ac3e45ded422d3615c1914a
jeffrey bradley <jnb2387@gmail.com>
2017-08-17 15:18:12 -0600

    'first'

---
+++ controls.js
@@ -0,0 +1,119 @@
+      //======Adding Controls to the Map.
+        //Add the Geocoder.
+        var geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken
+        });
+        map.addControl(geocoder);
+        //for the 3D button toggle
+        class PitchToggle { //create a class that has the pitch and bearing
+            constructor({ bearing = 0, pitch = 70, minpitchzoom = null }) {
+                this._bearing = bearing;
+                this._pitch = pitch;
+                this._minpitchzoom = minpitchzoom;
+            }
+
+            onAdd(map) {
+                this._map = map;
+                let _this = this;
+                this._btn = document.createElement('button');// create the 3d button
+                this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d';// set the class name for css styling
+                this._btn.type = 'button';
+                this._btn['aria-label'] = 'Toggle Pitch';//not sure
+                this._btn.onclick = function () { //when the button is clicked
+                    if (map.getPitch() === 0) {// if the pitch is 0 then run this
+                        let options = { pitch: _this._pitch, bearing: _this._bearing };// grab the pitch & bearing from the class constructor 
+                        if (_this._minpitchzoom && map.getZoom() > _this._minpitchzoom) {//dont really need
+                            options.zoom = _this._minpitchzoom;
+                        }
+                        map.easeTo(options);
+                        _this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-2d';//change button to show 2d after change
+                    } else {
+                        map.easeTo({ pitch: 0, bearing: 0 });// reset back to 2D if the pitch is not 0
+                        _this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d';//change button to show 3d after change
+                    }
+                };
+                this._container = document.createElement('div');// create a div
+                this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+                this._container.appendChild(this._btn);
+                return this._container;
+            }
+            onRemove() {
+                this._container.parentNode.removeChild(this._container);
+                this._map = undefined;
+            }
+        }
+
+        class Sidepanel { //create a class that will add the panel button to the map
+            onAdd(map) {
+                this._map = map;
+                let _this = this;
+                this._btn = document.createElement('button');// create the  button
+                this._btn.type = 'button';
+                this._btn.innerHTML = "Panel";
+                this._btn['aria-label'] = 'Toggle Pitch';//not sure
+                this._btn.onclick = function () { //when the button is clicked
+                    if ($("#sidepanel")) {// if the panel is showing then run this
+                        console.log("Sidepanel Toggled")
+                        $("#sidepanel").toggle()
+                    } else {
+                        console.log("something went wrong")
+                    }
+                };
+                this._container = document.createElement('div');// create a div
+                this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+                this._container.appendChild(this._btn);
+                return this._container;
+            }
+            onRemove() {
+                this._container.parentNode.removeChild(this._container);
+                this._map = undefined;
+            }
+        }
+
+
+        class Zoompanel { //create a class that will add the panel button to the map
+            onAdd(map) {
+                
+                this._map = map;
+                let _this = this;
+                this._btn = document.createElement('button');// create the  button
+                this._btn.type = 'button';
+                this._btn['aria-label'] = 'Toggle Pitch';//not sure
+                this._btn.onclick = function () { //when the button is clicked
+                   var zoomnum = map.getZoom()
+                };
+                this._btn.innerHTML = zoomnum;
+                this._container = document.createElement('div');// create a div
+                this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+                this._container.appendChild(this._btn);
+                return this._container;
+            }
+            onRemove() {
+                this._container.parentNode.removeChild(this._container);
+                this._map = undefined;
+            }
+        }
+
+
+        map.addControl(new Zoompanel('bottom-right'))
+        map.addControl(new Sidepanel('bottom-right'))
+        //Add the zoom and nav controls.
+        var nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, 'top-left');
+        // Add geolocate control to the map.
+        map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        }));
+          map.addControl(new PitchToggle({ minpitchzoom: 11 }));
+
+        //Add Scalebar.
+        map.addControl(new mapboxgl.ScaleControl({
+            maxWidth: 80,
+            unit: 'imperial'
+        }));
+        //=======End Controls
+
+        
---
+++ script.js
@@ -0,0 +1,389 @@
+
+//Set up the map canvas
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam5iMjM4NyIsImEiOiJjaW8zb2o2bDkwMWJudmJsempjaHBvc2hrIn0.gxSz_BeDIJlbDrKExBPaEQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-113.7505012, 48.5562206],
+    zoom: 8
+});
+
+
+// When the map loads then run all this.
+map.on('load', function () {
+    //When the user clicks on the map query every feature where the click was.
+    map.on('click', function (e) {
+        var features = map.queryRenderedFeatures(e.point);//set the click point feature to the variable.
+        let info = {};// create object to hold the data
+        let feature = features[0];// grab the first index of the features.
+        if (feature.properties) {
+            info.properties = feature.properties;// if the feature has properties set the properties to the info object
+        }
+        if (feature.layer) {// this is the map.add and map.source info.
+            info.layer = feature.layer;
+        }
+        //Display the query data in the pre tag.
+        document.getElementById('allfeatures').innerHTML = JSON.stringify(info, null, 2);
+    });
+
+    //=======Add point to when a Geocode Point is found   
+    //Adding the Source of just GeoJSON for the geocoder point
+    map.addSource('geocode-point', {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": []
+        }
+    });
+    // takes the source of the geocode-point and adds it to the map.
+    map.addLayer({
+        "id": "point",
+        "source": "geocode-point",
+        "type": "circle",
+        "paint": {
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+        }
+    });
+    // Listen for the `geocoder.input` event that is triggered when a user
+    // makes a selection and add a symbol that matches the result.
+    geocoder.on('result', function (ev) {
+        console.log(ev)
+        map.getSource('geocode-point').setData(ev.result.geometry);//grab the source set above and set the location to the click spot
+    });
+    //=====End Geocode Point
+
+    //======LAYERS
+    //Add 3D buildings layer.
+    map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': {
+                'type': 'identity',
+                'property': 'height'
+            },
+            'fill-extrusion-base': {
+                'type': 'identity',
+                'property': 'min_height'
+            },
+            'fill-extrusion-opacity': .6
+        }
+    });
+
+    //glacier trails Vector tile hosted in mapbox studio
+    map.addSource('trails', {
+        "type": "vector",
+        "url": 'mapbox://jnb2387.7b98tlt7'
+    })
+    map.addLayer({
+        "id": "trails",
+        'type': 'line',
+        'source': 'trails',
+        'source-layer': 'glaciertrailsgeojson',
+        "layout": {
+            'visibility': 'visible',
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": {
+                'property': 'Miles',
+                'stops': [
+                    [0, 'blue'],
+                    [3, 'gold'],
+                    [5, 'green'],
+                    [9, 'black'],
+                    [11, 'red']
+                ]
+            },
+
+            "line-width": 3
+        }
+    }, "road-label-small");
+
+    map.addLayer({
+        "id": "trail-hover",
+        "type": "line",
+        "source": "trails",
+        'source-layer': 'glaciertrailsgeojson',
+        "layout": {
+            'visibility': 'visible',
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            'line-color': 'white',
+            "line-width": 9,
+            'line-opacity': .7
+
+        },
+        "filter": ["==", "TRLNAME", ""]
+    }, 'trails');
+
+    //set a pop up on the trail layer
+    map.on('click', 'trails', function (e) {
+        map.flyTo({ center: e.lngLat });
+        new mapboxgl.Popup({ offset: [14, 0] })
+            .setLngLat(e.lngLat)
+            .setHTML('<h5><strong>' + e.features[0].properties.TRLNAME + '</strong></h5>  <b>' + e.features[0].properties.Miles + '</b><em> Miles</em>')
+            .addTo(map);
+    });
+
+    // When the user moves their mouse over the trail layer, we'll update the filter in
+    // the trails-hover layer to only show the matching trail, thus making a hover effect.
+    map.on("mouseover", "trails", function (e) {
+        map.getCanvas().style.cursor = 'pointer';
+        map.setFilter("trail-hover", ["==", "TRLNAME", e.features[0].properties.TRLNAME]);
+    });
+
+    // Reset the trail-hover layer's filter when the mouse leaves the layer.
+    map.on("mouseleave", "trails", function () {
+        map.getCanvas().style.cursor = '';
+        map.setFilter("trail-hover", ["==", "TRLNAME", ""]);
+    });
+
+    //Denver Demographics Layer
+    map.addSource('demo', {
+        "type": "vector",
+        "url": 'mapbox://jnb2387.3kzsjc32'
+    });
+    map.on('click', function (e) {//when the user moves the mouse
+        var trailinfo = map.queryRenderedFeatures(e.point, {//query where the user moved for features set it to a variable
+            layers: ['demo', 'trails']// the layers to query when the user moves
+        });
+        if (trailinfo.length > 0) {//if there is any features that are returned in the query set the info in the pd element.
+
+            document.getElementById('pd').innerHTML = '<h4><strong>' + trailinfo[0].properties.NAME + '</strong><h4>' +
+                '</em></h4> \n \n <p><strong><em>Length: </strong>' + trailinfo[0].properties.Miles + ' Miles' +
+                '</em></p> \n <p><strong><em>' + trailinfo[0].properties.TRLUSE +
+                '</br></br>' + trailinfo[0].properties.TRLCLASS;
+
+        } else {// if there are no features under the move location.
+            document.getElementById('pd').innerHTML = '<p>Hover over a Census Area</p>';
+        }
+    });
+    //3D layer with trailinfo
+    map.addLayer({
+        "id": "demo",
+        'type': 'fill-extrusion',
+        "source": "demo",
+        'source-layer': 'census_neighborhood_demograph-bnbntm',
+        "layout": {
+            'visibility': 'visible',
+        },
+        "paint": {
+            'fill-extrusion-base': 0,
+            'fill-extrusion-color':
+            {
+                'property': 'POPULATION',
+                'stops': [
+                    [0, 'blue'],
+                    [2000, 'gold'],
+                    [5000, 'green'],
+                    [9000, 'black'],
+                    [15169, 'red']
+                ]
+
+            },
+            'fill-extrusion-height': {
+                'type': 'identity',
+                'property': 'POPULATION'
+            },
+
+            'fill-extrusion-opacity': .9,
+        }
+    });
+
+});// end map load
+
+$(document).ready(function () {
+    $("#getAjax").click(function () {
+        var ajaxfeatures;
+        $.ajax({
+            method: 'GET',
+            url: 'glacierpoints.geojson',
+            crossDomain: true,
+            dataType: 'json',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            success: function (response) {
+                //Add 3D another layer.
+                var ajaxLayer = map.addLayer({
+
+                    "id": "my3d",
+                    'type': 'fill-extrusion',
+                    'source': {
+                        'type': 'geojson',
+                        'data': response
+                    },
+                    "layout": {
+                        'visibility': 'visible',
+                    },
+                    "paint": {
+                        'fill-extrusion-base': 0,
+                        'fill-extrusion-color':
+                        {
+                            'property': 'Miles',
+                            'stops': [
+                                [0, 'blue'],
+                                [2000, 'gold'],
+                                [5000, 'green'],
+                                [9000, 'black'],
+                                [15169, 'red']
+                            ]
+                        },
+                        'fill-extrusion-height': {
+                            'type': 'identity',
+                            'property': 'Miles'
+                        },
+                        'fill-extrusion-opacity': .9,
+                    }
+                });
+
+
+                console.log(response);
+                ajaxfeatures = response;
+
+            }
+        });
+    })
+    $("#getcamp").click(function () {
+        $('#getcamp').html('Remove Points')
+        $('#legend').toggle()
+
+        console.log('getting POI\'s')
+        var ajaxfeatures;
+        $.ajax({
+            method: 'GET',
+            url: 'glacierpoints.geojson',
+            crossDomain: true,
+            dataType: 'json',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            success: function (response) {
+                // console.log(response)
+                map.addSource('glacierpoints', {
+                    'type': "geojson",
+                    "data": response,
+                    'cluster': true,
+                    'clusterMaxZoom': 15, // Max zoom to cluster points on
+                    'clusterRadius': 20 // Use small cluster radius for the heatmap look
+                })
+
+                var layers = [
+                    [0, 'blue'],
+                    [5, 'orange'],
+                    [20, 'red']
+                ]
+
+                layers.forEach(function (layer, i) {
+                    map.addLayer({
+                        "id": "cluster-" + i,
+                        "type": "circle",
+                        "source": "glacierpoints",
+                        "paint": {
+                            "circle-color": layer[1],
+                            "circle-radius": 70,
+                            "circle-blur": 1 // blur the circles to get a heatmap look
+                        },
+                        "filter": i === layers.length - 1 ?
+                            [">=", "point_count", layer[0]] :
+                            ["all",
+                                [">=", "point_count", layer[0]],
+                                ["<", "point_count", layers[i + 1][0]]]
+                    }, 'waterway-label');
+                });
+
+                map.addLayer({
+                    'id': 'glacierpoints',
+                    'source': 'glacierpoints',
+                    'type': 'circle',
+                    "layout": {
+                        'visibility': 'visible',
+                    },
+                    'paint': {
+                        // make circles larger as the user zooms from z12 to z22
+                        'circle-radius': {
+                            'base': 1.75,
+                            'stops': [[12, 5], [22, 180]]
+                        },
+                        "circle-blur": 1,
+                        // color circles by POI type, using data-driven styles
+                        'circle-color': {
+                            'property': 'POITYPE',
+                            'type': 'categorical',
+                            'stops': [
+                                ['Trailhead', '#fbb03b'],
+                                ['Campground', '#223b53'],
+                                ['Viewpoint', '#e55e5e'],
+                                ['Cabin', '#3bb2d0'],
+                                ['Lodging', 'blue'],
+                                ['Picnic Area', 'red'],
+                                ['Entrance Station', 'green'],
+                                ['Point of Interest', 'orange'],
+                                ['Food Service', 'yellow'],
+                                ['Restroom', 'pink'],
+                                ['Tower', 'purple'],
+                                ['Other', '#ccc']]
+                        }
+                    },
+                    "filter": ["!=", "cluster", true]
+                });
+            }
+
+        })
+        //set a pop up on the trail layer
+        map.on('click', 'glacierpoints', function (e) {
+            map.flyTo({ center: e.lngLat });
+            new mapboxgl.Popup({ offset: [14, 0] })
+                .setLngLat(e.lngLat)
+                .setHTML('<h5><strong>' + e.features[0].properties.POINAME + '</strong></h5>  <b>' + e.features[0].properties.POITYPE + '</b>')
+                .addTo(map);
+        });
+    })
+    // var visibility= map.getLayoutProperty('glacierpoints','visibility');
+    //     $('#demobtn').click(function () {
+    //         console.log("Demo Button Clicked");
+
+    //         var visibility = map.getLayoutProperty('demo', 'visibility')
+    //         if (visibility === 'visible') {
+    //             map.setLayoutProperty('demo', 'visibility', 'none');
+    //         } else {
+    //             map.setLayoutProperty('demo', 'visibility', 'visible');
+    //         }
+    //     });
+
+
+
+    //======controls for the trail layer
+    //button to toggle trail layer visibility
+    $('#trailbtn').click(function () {
+        console.log("Trail Button Clicked");
+        var visibility = map.getLayoutProperty('trails', 'visibility')
+        if (visibility === 'visible') {
+            map.setLayoutProperty('trails', 'visibility', 'none');
+        } else {
+            map.setLayoutProperty('trails', 'visibility', 'visible');
+        }
+    });
+    var changeColor = $('#btn').click(function () {
+        var color = $('#myinput').val()
+        map.setPaintProperty('trails', 'line-color', color);
+    });
+    //=======end trail layer controls            
+    var urbanbtn = $('#urbanbtn');
+    urbanbtn.click(function () {
+
+        var visible = map.getLayoutProperty('camps', 'visibility');
+        visible == "none" ? map.setLayoutProperty('camps', 'visibility', 'visible') : map.setLayoutProperty('camps', 'visibility', 'none')
+    });
+});
+
